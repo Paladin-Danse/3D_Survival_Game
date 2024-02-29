7dcbccdbb27d3e896e700378db6f564f5960fdc8
footprintbeaver <tkdlzhmk3@gmail.com>
2024-02-29 20:12:44 +0900

    [feat] UI추가 및 명칭변경

--- Assets/Scriptable Objects/Scripts/KitData.cs
+++ Assets/Scriptable Objects/Scripts/KitData.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum ToolType
+public enum KitType
 {
     Table,
     Alchemy,
     Casting,
-    Cooking
+    CookPot
 }
 
-[CreateAssetMenu(fileName = "Tool", menuName = "New Tool")]
-public class ToolData : ScriptableObject
+[CreateAssetMenu(fileName = "Kit", menuName = "New Kit")]
+public class KitData : ScriptableObject
 {
     [Header("Info")]
     public string displayName;
     public string description;
-    public ToolType type;
+    public KitType type;
 }
