0da4167eade0cbfeba37239333d907f8db0db820
이승준 <37408775+Paladin-Danse@users.noreply.github.com>
2024-03-04 00:04:29 +0900

    <fix> 곰이 공격을 받지 않는 문제 해결. 곰이 일으키는 자잘한 버그 수정.

--- Assets/Scripts/NPC/StateMachine/AnimalAttackState.cs
+++ Assets/Scripts/NPC/StateMachine/AnimalAttackState.cs
@@ -47,7 +47,6 @@ public class AnimalAttackState : AnimalBaseState
             else
             {
                 stateMachine.ChangeState(stateMachine.fleeState);
-                //SetState(AIState.Fleeing);
             }
         }
         else
--- Assets/Scripts/NPC/StateMachine/StateMachine.cs
+++ Assets/Scripts/NPC/StateMachine/StateMachine.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public abstract class StateMachine
 {
     protected IState currentState;
-
     public void ChangeState(IState newState)
     {
+        
         currentState?.Exit();
         currentState = newState;
         currentState?.Enter();
+        Debug.Log(currentState.ToString());
     }
 
     public void HandleInput()
